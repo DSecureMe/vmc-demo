acd75b3adaff0596793a684b36597dc51faf23be
Michał Walkowski <mi.walkowski@gmail.com>
2022-02-15 13:46:25 +0100

    FIX: docker container names

--- config/demo_data.sh
+++ config/demo_data.sh
@@ -1,25 +1,25 @@
 echo "Elastalert: Create Indexes"
-docker exec -it compose_elastalert_1 /usr/bin/elastalert-create-index --config /opt/elastalert/config.yaml
+docker exec -it elastalert /usr/bin/elastalert-create-index --config /opt/elastalert/config.yaml
 
 echo "HIVE: create database (superuser login: admin, password: admin)"
-docker exec -it compose_elasticsearch_1 chmod +x /test_data/load.sh
-docker exec -it compose_elasticsearch_1 /test_data/load.sh
+docker exec -it elasticsearch chmod +x /test_data/load.sh
+docker exec -it elasticsearch /test_data/load.sh
 
 echo "Ralph: Make migrations"
-docker exec -it compose_web_1 ralphctl migrate
+docker exec -it ralph_web ralphctl migrate
 
 echo "Ralph: Load demo data  (login: ralph, password: ralph)"
-docker exec -it compose_web_1 ralphctl demodata
-docker exec -it compose_web_1 ralphctl sitetree_resync_apps
-docker exec -it compose_web_1 python3 /test_data/generate_ips.py
+docker exec -it ralph_web ralphctl demodata
+docker exec -it ralph_web ralphctl sitetree_resync_apps
+docker exec -it ralph_web python3 /test_data/generate_ips.py
 
 echo "VMC: Load data (superuser login: admin, password: admin)"
-docker exec -it compose_admin_1 python3.7 -m vmc loaddata /test_data/demo_data.json
-docker exec -it compose_admin_1 python3.7 -m vmc create_index
+docker exec -it vmc_admin python3.7 -m vmc loaddata /test_data/demo_data.json
+docker exec -it vmc_admin python3.7 -m vmc create_index
 
 echo "Kibana: Import Sample Dashboards and KPIs"
-docker exec -it compose_kibana_1 chmod +x /test_data/load.sh
-docker exec -it compose_kibana_1 /test_data/load.sh
+docker exec -it kibana chmod +x /test_data/load.sh
+docker exec -it kibana /test_data/load.sh
 
 echo "VMC: Prepare demo data"
-docker exec -it compose_admin_1 python3.7 -W ignore /test_data/generate_vulns.py
+docker exec -it vmc_admin python3.7 -W ignore /test_data/generate_vulns.py
